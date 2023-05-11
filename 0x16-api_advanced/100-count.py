f5c50bdcdefcf7cd20d6c2a3eb29f205e0852d95
uwenorjeff <106762251+uwenorjeff@users.noreply.github.com>
2023-05-11 13:34:23 +0100

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,61 +1,50 @@
 #!/usr/bin/python3
-"""
-Querries the Reddit API and returns a list of the titles of all hot posts
-listed for a given subreddit
-"""
+""" Module for a function that queries the Reddit API recursively."""
 
-import json
+
 import requests
 
 
-def count_words(subreddit, word_list, hot_list=[], after=None):
-    """Return a list of the titles of all hot posts listed for a subreddit"""
-    headers = {"user-agent": "holberton"}
-    params = {"after": after}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    subdata = requests.get(url, headers=headers, params=params)
-    if subdata.status_code != 200:
-        return None
-    data = json.loads(subdata.text).get('data').get('children')
-    after = json.loads(subdata.text).get('data').get('after')
-    if data is None:
-        if len(hot_list) == 0:
-            print("")
-            return
-        word_count = {}
-        word_list = list(set([word.lower() for word in word_list]))
+def count_words(subreddit, word_list, after='', word_dict={}):
+    """ A function that queries the Reddit API parses the title of
+    all hot articles, and prints a sorted count of given keywords
+    (case-insensitive, delimited by spaces.
+    Javascript should count as javascript, but java should not).
+    If no posts match or the subreddit is invalid, it prints nothing.
+    """
+
+    if not word_dict:
         for word in word_list:
-            word_count[word] = 0
-        for word in word_list:
-            for title in hot_list:
-                for t in title.split():
-                    if word == t.lower():
-                        word_count[word] = word_count[word] + 1
-        sort_word_count = sorted(word_count.items(), key=lambda x: x[1],
-                                 reverse=True)
-        for i in sort_word_count:
-            if i[1] > 0:
-                print("{}: {}".format(i[0], i[1]))
-    else:
-        for item in data:
-            hot_list.append(item.get('data').get('title'))
+            if word.lower() not in word_dict:
+                word_dict[word.lower()] = 0
+
     if after is None:
-        if len(hot_list) == 0:
-            print("")
-            return
-        word_count = {}
-        word_list = list(set([word.lower() for word in word_list]))
-        for word in word_list:
-            word_count[word] = 0
-        for word in word_list:
-            for title in hot_list:
-                for t in title.split():
-                    if word.lower() == t.lower():
-                        word_count[word] = word_count[word] + 1
-        sort_word_count = sorted(word_count.items(), key=lambda x: x[1],
-                                 reverse=True)
-        for i in sort_word_count:
-            if i[1] > 0:
-                print("{}: {}".format(i[0], i[1]))
-    else:
-        return count_words(subreddit, word_list, hot_list, after)
+        wordict = sorted(word_dict.items(), key=lambda x: (-x[1], x[0]))
+        for word in wordict:
+            if word[1]:
+                print('{}: {}'.format(word[0], word[1]))
+        return None
+
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    header = {'user-agent': 'redquery'}
+    parameters = {'limit': 100, 'after': after}
+    response = requests.get(url, headers=header, params=parameters,
+                            allow_redirects=False)
+
+    if response.status_code != 200:
+        return None
+
+    try:
+        hot = response.json()['data']['children']
+        aft = response.json()['data']['after']
+        for post in hot:
+            title = post['data']['title']
+            lower = [word.lower() for word in title.split(' ')]
+
+            for word in word_dict.keys():
+                word_dict[word] += lower.count(word)
+
+    except Exception:
+        return None
+
+    count_words(subreddit, word_list, aft, word_dict)
